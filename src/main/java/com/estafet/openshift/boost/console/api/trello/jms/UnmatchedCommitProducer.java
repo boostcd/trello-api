084dd56f5e1501f06bb32b097cdf38e5b4625aca
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-03-05 11:22:58 +0000

    cleaned up producer

--- src/main/java/com/estafet/openshift/boost/console/api/trello/jms/UnmatchedCommitProducer.java
+++ src/main/java/com/estafet/openshift/boost/console/api/trello/jms/UnmatchedCommitProducer.java
@@ -1,14 +1,10 @@
 package com.estafet.openshift.boost.console.api.trello.jms;
 
-import com.estafet.openshift.boost.messages.model.UnmatchedCommitMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessagePostProcessor;
 import org.springframework.stereotype.Component;
 
-import javax.jms.JMSException;
-import javax.jms.Message;
-import java.util.UUID;
+import com.estafet.openshift.boost.messages.model.UnmatchedCommitMessage;
 
 @Component
 public class UnmatchedCommitProducer {
@@ -18,12 +14,7 @@ public class UnmatchedCommitProducer {
 
     public void sendMessage(UnmatchedCommitMessage message) {
         jmsTemplate.setPubSubDomain(true);
-        jmsTemplate.convertAndSend("unmatched.commit.topic", message.toJSON(), new MessagePostProcessor() {
-            @Override
-            public Message postProcessMessage(Message message) throws JMSException {
-                message.setStringProperty("message.event.interaction.reference", UUID.randomUUID().toString());
-                return message;
-            }
-        });
+        jmsTemplate.convertAndSend("unmatched.commit.topic", message.toJSON());
     }
+    
 }
